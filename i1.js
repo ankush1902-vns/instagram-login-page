a172176906a8c4ae5963cbc42674b9127fcf99cf
ankush1902-vns <ankushrajvns2005@gmail.com>
2025-02-15 18:10:51 +0530

    Add files via upload

---
+++ i1.js
@@ -0,0 +1,30 @@
+var count = 0 ;
+function change()
+{
+    count++ ;
+   if (count == 1)
+   {
+        photo4.src = "Daco_4528487.png" ;
+   }
+   else if (count == 2)
+   {
+        photo4.src = "k1.png" ;
+   }
+   else
+   {
+         photo4.src = "l1.png" ;
+         count = 0 ;
+   }
+}
+function work()
+ {
+    document.getElementsByName("write").style.color = "red" ;
+ }
+function bright()
+ {
+    open("https://play.google.com/store/apps/details?id=com.instagram.android&referrer=ig_mid%3D626DF313-78BE-4F8A-8464-8F412F70DD52%26utm_campaign%3DloginPage%26utm_content%3Dlo%26utm_source%3Dinstagramweb%26utm_medium%3Dbadge%26original_referrer%3Dhttps%3A%2F%2Fwww.google.com%2F");
+ }
+ function letting()
+ {
+     open("https://apps.microsoft.com/home?hl=en-US&gl=US");
+ }
